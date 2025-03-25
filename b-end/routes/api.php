3c586df657195fb6afcc6ac587fb05e5c22cc592
Delgado_C <delgadocharles1614@gmail.com>
2025-03-25 14:52:51 +0800

    api checking

--- b-end/routes/api.php
+++ b-end/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+/*use Illuminate\Support\Facades\Auth; delete mo nalang*/
 /*
 |--------------------------------------------------------------------------
 | API Routes
