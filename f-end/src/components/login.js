f9745eaafa8f12efb1dfd7b24be3bf8062ed6c42
Hans-Axle <consuelohansaxle07@gmail.com>
2025-04-30 09:48:42 +0800

    fix: Update comment for clarity in Login component

--- f-end/src/components/login.js
+++ f-end/src/components/login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-//this is for login page
+//this is for login
 const Login = ({ onLogin }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
