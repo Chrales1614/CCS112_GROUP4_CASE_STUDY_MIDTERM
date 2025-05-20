32c6f227accbe65dd9c944f8e93d12e80a3b5d7b
ren-lyn <concinarenelyn86@gmail.com>
2025-05-21 03:20:23 +0800

    dashboard-update

--- f-end/src/components/dashboard.js
+++ f-end/src/components/dashboard.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+//Dashboard
 const Dashboard = ({ onLogout }) => {
   const navigate = useNavigate();
   const [projects, setProjects] = useState([]);
